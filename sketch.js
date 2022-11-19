f305b5ba49ab01ff8ac851a8bdd174c1c49b5e28
logan974 <33648275+logan974@users.noreply.github.com>
2022-11-19 18:01:56 +0400

    Debut de la branche de test

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
+var pos = {
+    x: 100,
+    y: 160,
+    s: 50,
+    s2: 50
+}
+
+var v = 3
+
+
 function setup() {
-  // put setup code here
+    createCanvas(400, 400);
 }
 
 function draw() {
-  // put drawing code here
+    background(220);
+    ellipse(pos.x, pos.y, pos.s, pos.s2)
+    pos.y += v
+    if (pos.y >= height - pos.s / 2) {
+
+        pos.s2 -= 1
+        v = v - 0.4
+
+    } else if (pos.y <= 0 + pos.s / 2) {
+        pos.s2 -= 1
+        v = v + 0.4
+    } else if (pos.y <= height + pos.s / 2 || pos.y >= 0 - pos.s / 2) {
+        if (pos.s2 <= 50) {
+            pos.s2 += 1;
+        }
+    }
+
 }
