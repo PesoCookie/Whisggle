9af66b24534f26281b52896ff609e2ddb7f35a75
Matthieu <matthieu@matthieu-dev.xyz>
2022-11-21 15:43:41 +0000

    Nettoyage de code (huge)

--- code/ball.js
+++ code/ball.js
@@ -1,27 +1,46 @@
-class ball {
+/**
+ * @description Balle
+ * @property {Point2d} position Position de la balle dans un plan 2d
+ * @property {Point2d} speed Vitesse de la balle à chaque mouvement, défini en interne avec launch()
+ * @property {number} radius Rayon de la balle
+ * @property {string} color Couleur de la balle
+ */
+class Ball {
+    /**
+     * @constructor
+     * @description Crée une nouvelle instance d'une balle
+     */
     constructor() {
-        this.x = width / 2;
-        this.y = 20;
-        this.vx = 0;
-        this.vy = 0;
-        this.r = 20;
-        this.color = 'rgb(170,169,173)'
+        this.position = new Point2d(width / 2, 20);
+        this.speed = new Point2d(0, 0);
+        this.radius = 20;
+        this.color = 'rgb(170,169,173)';
     }
+    /**
+     * @description Desinne la balle
+     */
     draw() {
-        fill(this.color)
-        circle(this.x, this.y, this.r)
-        fill('white')
-        circle(this.x - this.r / 5, this.y - this.r / 5, this.r / 3)
+        fill(this.color);
+        circle(this.position.x, this.position.y, this.radius);
+        fill('white');
+        // Dessine un petit cercle qi représente la réflextion de la lumière.
+        circle(this.position.x - this.radius / 5, this.position.y - this.radius / 5, this.radius / 3);
     }
-    launch(testX, testY) {
-        this.vx = (testX - this.x) / 30
-        this.vy = (testY - this.y) / 30
-        en_cour = false
+    /**
+     * @description Initialise le mouvement de la balle selon la position du point donné (généralement position du curseur)
+     * @param {Point2d} point Position du point
+     */
+    launch(point) {
+        this.speed.x = (point.x - this.position.x) / 30
+        this.speed.y = (point.y - this.position.y) / 30
+        en_cours = true
     }
-    mouve() {
-        this.x += this.vx;
-        this.y += this.vy;
+    /**
+     * @description Mouvement de la balle
+     */
+    move() {
+        this.position.add(this.speed);
         this.rebond();
     }
-    rebond() {}
+    rebond() { }
 }
--- code/sketch.js
+++ code/sketch.js
@@ -1,16 +1,23 @@
+/**
+ * @description Dessine une frame
+ */
 function draw() {
     background(100);
-
+    // Dessine le background
     image(fond, 0, 0, width, height, 0, 0, fond.width, fond.height)
     image(bord, 0, 0, width, height, 0, 0, bord.width, bord.height, CONTAIN, LEFT);
     circle(width / 2, 0, width / 7);
-    main.draw();
-    main.mouve();
+
+    balle_principale.draw();
+    balle_principale.mouve();
 }
 
+/**
+ * @description Exécuté à chaque clic
+ */
 function mousePressed() {
-    if (en_cour == true) {
-        main.launch(mouseX, mouseY);
+    if (en_cours == true) {
+        balle_principale.launch(mouseX, mouseY);
     }
 
 }
