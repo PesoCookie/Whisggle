b47af9459892bbf2e9939c607bc0a019eb09bf55
PesoCookie <metastoan@gmail.com>
2022-11-21 09:50:23 +0400

    Update sketch.js

--- code/sketch.js
+++ code/sketch.js
@@ -1,9 +1,9 @@
 function draw() {
-    //background(100);
+    background(100);
 
-    //image(fond, 0 ,0 , width, height, 0, 0, fond.width, fond.height)
-    //image(bord, 0, 0, width, height, 0, 0, bord.width, bord.height, CONTAIN, LEFT);
-    //circle(width / 2, 0, width / 7);
+    image(fond, 0, 0, width, height, 0, 0, fond.width, fond.height)
+    image(bord, 0, 0, width, height, 0, 0, bord.width, bord.height, CONTAIN, LEFT);
+    circle(width / 2, 0, width / 7);
     main.draw();
     main.mouve();
 }
