03426868277b93985082c11c9d5eff8f14e683a6
logan974 <33648275+logan974@users.noreply.github.com>
2022-11-21 09:43:18 +0400

    Update setup.js

--- code/setup.js
+++ code/setup.js
@@ -2,6 +2,12 @@
 let bord
 let fond
 
+var en_cour, main;
+// en_cour vérifie si la balle en sur l'écran pour éviter de lancer plusieur balles
+// true signifie que la balle n'est pas la, false si elle est là
+// main correspond à la balle principale
+
+
 function preload() {
     bord = loadImage('assets/bord.png');
     fond = loadImage('assets/fond.jpg');
