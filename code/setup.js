3416619427406e9b6c6378ad1f9c8eb3287e752a
logan974 <33648275+logan974@users.noreply.github.com>
2022-11-21 09:37:03 +0400

    Creation du début d'interface

--- code/setup.js
+++ code/setup.js
@@ -1,3 +1,11 @@
+let bord
+let fond
+
+function preload() {
+    bord = loadImage('assets/bord.png');
+    fond = loadImage('assets/fond.jpg');
+  }
+
 function setup() {
     // Creation du canvas 
     createCanvas(1270, 600);
--- code/sketch.js
+++ code/sketch.js
@@ -1,4 +1,6 @@
 function draw() {
     background(100);
+    image(fond, 0 ,0 , width, height, 0, 0, fond.width, fond.height)
+    image(bord, 0, 0, width, height, 0, 0, bord.width, bord.height, CONTAIN, LEFT);
     circle( width / 2 , 0 , width / 7);
 }   
