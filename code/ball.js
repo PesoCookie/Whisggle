de0b1de4c0fdbd812a52fc57bf434175586e845a
PesoCookie <metastoan@gmail.com>
2022-11-21 08:52:39 +0400

    Update ball.js

--- code/ball.js
+++ code/ball.js
@@ -4,6 +4,13 @@ class ball {
         this.y = 0;
         this.vx = 0;
         this.vy = 0;
+        this.r = 20;
         this.color = 'rgb(170,169,173)'
+    }
+    draw() {
+        fill(this.color)
+        circle(this.x, this.y, this.r)
+        fill('white')
+        circle(this.x - this.r / 5, this.y - this.r / 5, this.r / 3)
     }
 }
