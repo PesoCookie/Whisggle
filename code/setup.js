2ff636f28f51aa72d565b5e3a1f1995ee06f6ddd
ouijesais <118590845+ouijesais@users.noreply.github.com>
2022-11-28 21:06:14 +0400

    luc 1
    yfut

--- code/setup.js
+++ code/setup.js
@@ -36,6 +36,18 @@ let en_cours = false;
  let currentScore;
  /**
  * @global
+ * @type {nbBallesligne}
+ * @name nbBallesligne
+ */
+  let nbBallesligne;
+/**
+ * @global
+ * @type {nbBallesColonne}
+ * @name nbBallesColonne
+ */
+let nbBallesColonne;
+ /**
+ * @global
  * @type {int}
  * @name maxScore
  */
@@ -72,9 +84,11 @@ function setup() {
     textSize(window.innerWidth / 35);
     textFont(fontImpact);
 
-    maxScore = 10 // Valeur temporaire, a faire selon le nombre d'obstacle max  
-
+    nbBallesligne = 30
+    nbBallesColonne = 5
+    maxScore = nbBallesColonne*nbBallesligne // Valeur temporaire, a faire selon le nombre d'obstacle max  
     balle_principale = new Ball();
     currentScore = new score(0);
+    tableau();
     
 }
